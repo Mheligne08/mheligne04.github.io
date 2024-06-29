b7e91f146141b2a304ca89a3ddc34e172f4891f8
Mheligne08 <mhel_021199@binalatongan.edu.ph>
2024-06-29 12:07:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+var producti document.getElementById("product1")
 
+var qtyl document.getElementById("qtyl")
+
+var pricel document.getElementById("pricel")
+
+var product2 document.getElementById("product2")
+
+var qty2 document.getElementById("qty2")
+
+var price2 document.getElementById("price2")
+
+var carta document.getElementById("carts")
+
+var total document.getElementById("total") var cash document.getElementById("cash")
+
+var change document.getElementById("change")
+
+function addOrder() {
+
+carts.textContent=""
+
+if (parseFloat(qtyl.value) > 0){
+
+var order qty1.value.toString() + pc/s x pricel.textContent //carts.textContent + carts.value.toString() + "\n"; product1.textContent Php (parseFloat(qtyl.value) *parseFloat(pricel.textConten
+
+carts.textContent order
+
+if (parseFloat(qty2.value) > 0) {
+
+var order qty2.value.toString() + pc/s x //carts.textContent += carts.value.toString() price2.textContent "\n"; product2.textContent Php (parseFloat (qty2.value) *parseFloat (price2.textConten
+
+carte.textContent
+
+qtyl.addEventListener("keyup", addOrder): qty2.addEventListener("keyup", addOrder):
+
+order
