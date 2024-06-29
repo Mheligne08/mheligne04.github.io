13c5e6dd4b6de6022866dc4a101b5224a1ed9123
Mheligne08 <mhel_021199@binalatongan.edu.ph>
2024-06-28 21:17:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -18,10 +18,9 @@ var order qty1.value.toString() +' pc/s x ' pricel.textContent + '______' + prod
 carts.textContent order
 
 if (parseFloat(qty2.value) > 0) {
-
-var order qty2.value.toString() + pc/s x //carts.textContent += carts.value.toString() price2.textContent "\n"; product2.textContent Php (parseFloat (qty2.value) *parseFloat (price2.textConten
-
-carte.textContent
+var order qty2.value.toString() +' pc/s x ' price2.textContent + '______' + product2.textContent + '______' Php + (parseFloat(qty2.value) *parseFloat(price2.textContent)) + '/n'
+//carts.textContent + carts.value.toString() + "\n"; 
+carts.textContent order
 
 qtyl.addEventListener("keyup", addOrder): qty2.addEventListener("keyup", addOrder):
 
