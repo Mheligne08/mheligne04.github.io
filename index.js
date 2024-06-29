a3e8d0bc41ce447989f2fe9518153cde8ceda1df
Mheligne08 <mhel_021199@binalatongan.edu.ph>
2024-06-28 21:15:38 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
 var producti document.getElementById("product1")
-
 var qtyl document.getElementById("qtyl")
-
-var pricel document.getElementById("pricel")
+var pricel document.getElementById("price1")
 
 var product2 document.getElementById("product2")
-
 var qty2 document.getElementById("qty2")
-
 var price2 document.getElementById("price2")
 
-var carta document.getElementById("carts")
-
+var carts document.getElementById("carts")
 var total document.getElementById("total") var cash document.getElementById("cash")
-
 var change document.getElementById("change")
 
 function addOrder() {
-
 carts.textContent=""
-
 if (parseFloat(qtyl.value) > 0){
-
-var order qty1.value.toString() + pc/s x pricel.textContent //carts.textContent + carts.value.toString() + "\n"; product1.textContent Php (parseFloat(qtyl.value) *parseFloat(pricel.textConten
-
+var order qty1.value.toString() +' pc/s x ' pricel.textContent + '______' + product1.textContent + '______' Php + (parseFloat(qtyl.value) *parseFloat(pricel.textContent)) + '/n'
+//carts.textContent + carts.value.toString() + "\n"; 
 carts.textContent order
 
 if (parseFloat(qty2.value) > 0) {
