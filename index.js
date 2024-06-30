14919eb0f0cde2fb293f88f919a13f5865407502
Mheligne08 <mhel_021199@binalatongan.edu.ph>
2024-06-30 23:40:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,19 @@ function addOrder() {
     if (parseFloat(qty3.value) > 0) {
         var order3 = qty3.value.toString() + " pcs x " + product3.textContent + " - Php " + (parseFloat(qty3.value) * parseFloat(price3.textContent)).toFixed(2) + "\n";
         carts.textContent += order3;
-}
+    }
+    if (parseFloat(qty4.value) > 0) {
+        var order4 = qty4.value.toString() + " pcs x " + product4.textContent + " - Php " + (parseFloat(qty4.value) * parseFloat(price4.textContent)).toFixed(2) + "\n";
+        carts.textContent += order4;
+    }
+    if (parseFloat(qty5.value) > 0) {
+        var order5 = qty5.value.toString() + " pcs x " + product5.textContent + " - Php " + (parseFloat(qty5.value) * parseFloat(price5.textContent)).toFixed(2) + "\n";
+        carts.textContent += order5;
+    }
+    if (parseFloat(qty6.value) > 0) {
+        var order6 = qty6.value.toString() + " pcs x " + product6.textContent + " - Php " + (parseFloat(qty6.value) * parseFloat(price6.textContent)).toFixed(2) + "\n";
+        carts.textContent += order6;
+    }
     updateTotal(); // Update total after adding orders
 }
 
@@ -35,11 +47,14 @@ function updateTotal() {
     var total = 0;
 
     total += parseFloat(qty1.value) * parseFloat(price1.textContent);
-
     total += parseFloat(qty2.value) * parseFloat(price2.textContent);
     total += parseFloat(qty3.value) * parseFloat(price3.textContent);
+    total += parseFloat(qty4.value) * parseFloat(price4.textContent);
+    total += parseFloat(qty5.value) * parseFloat(price5.textContent);
+    total += parseFloat(qty6.value) * parseFloat(price6.textContent);
     
-
+    
+    
     totalInput.value = total.toFixed(2); // Update total input field
     calculateChange(); // Calculate change after updating total
 }
@@ -59,4 +74,7 @@ function calculateChange() {
 qty1.addEventListener("keyup", addOrder);
 qty2.addEventListener("keyup", addOrder);
 qty3.addEventListener("keyup", addOrder);
+qty4.addEventListener("keyup", addOrder);
+qty5.addEventListener("keyup", addOrder);
+qty6.addEventListener("keyup", addOrder);
 cashInput.addEventListener("input", calculateChange);
